4b52d8f5b02d0dd9681c7ac8a551e067b04f0ab8
Luis-Sena-Av <luchito721@hotmail.com>
2023-06-05 12:28:57 -0500

    final

--- src/components/InputId.jsx
+++ src/components/InputId.jsx
@@ -1,4 +1,6 @@
 import React, { useRef } from 'react'
+import { useGetapi } from '../hooks/useGetapi'
+import { useState,useEffect } from 'react'
 
 export const InputId = ({setlocationId}) => {
 
@@ -6,18 +8,55 @@ export const InputId = ({setlocationId}) => {
 
     const handlesutmit=(e)=>{
     e.preventDefault()
-    if(inpid.current.value.trim()!==""){
-        setlocationId(inpid.current.value)
+      let id=0
+      if(inpid.current.value.trim()!==""){
+
+        if(locationsName.includes(inpid.current.value)){
+          id=locationsName.indexOf(inpid.current.value)+1
+        }else{
+          id=inpid.current.value
+        }
+        setlocationId(id)
+      }
     }
-    
-    }
+      const [url, seturl] = useState("https://rickandmortyapi.com/api/location?page=1")
+      const [location1,getApi]=useGetapi(url)
+      const [locationsName,setlocationsName]=useState([])
+      
+
+      useEffect(()=>{
+        if(location1?.info.next){
+          seturl(location1?.info.next)
+        }
+      },[location1])
+
+      useEffect(() => {
+        getApi()
+      }, [url])
+
+      useEffect(()=>{
+        if(location1){
+          for (const item of location1?.results) {
+            locationsName.push(item.name)
+          }
+        }
+      },[location1])
+
 
   return (
     <>
-        <form className='form' onSubmit={handlesutmit}>
-            <input className='intro' type="text" ref={inpid}/>
-            <input className='enviar' type="submit" />
-        </form>
+      <form className='form' onSubmit={handlesutmit}>
+        <input className='intro' type="text" list='nombrelocaciones' ref={inpid}/>
+        <input className='enviar' type="submit" />
+        <datalist id='nombrelocaciones'>
+          {
+            locationsName.map((name,item)=>
+              <option value={name} key={item}></option>
+              )
+          }
+        </datalist>
+      </form>
+
     </>
   )
 
